1b41ce9c58029c83ca271eadcbf6f861f86f534a
staytheknight <staytheknight@gmail.com>
2022-08-20 13:37:19 -0600

    Added level end text

--- AB GameJam 2022/Assets/Scripts/EndTrigger.cs
+++ AB GameJam 2022/Assets/Scripts/EndTrigger.cs
@@ -7,12 +7,12 @@ public class EndTrigger : MonoBehaviour
     public GameManager gameManager;
 
     // When the object is triggered (through collision) 
-    void OnTriggerEnter()
+    void OnTriggerEnter2D()
     {
 
         Debug.Log("Level Finished");
         gameManager.CompleteLevel();            //Call the complete level function of the game manager
-        
+
     }
 
 }
